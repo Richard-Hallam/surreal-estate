b5efef1a4ff1cd7a9ae3526a1a5d8b9be32d9941
Richard Hallam <richardhallam25@gmail.com>
2023-10-26 21:22:36 +0100

    navbar css tweaks

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,12 @@ import '../styles/Navbar.css';
 
 const NavBar = (props) => {
   return (
-    <div className="NavBar">
+    <div className="navbar">
       <img src="src/logo.png" alt="surreal estate logo" />
+      <ul className='navbar-links'>
+        <li className='navbar-links-item'>View Properties</li>
+        <li className='navbar-links-item'>Add Properties</li>
+      </ul>
     </div>
   );
 };
